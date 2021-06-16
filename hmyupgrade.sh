eec95be9a956d8d12b7dd5b09acf45ab40ca20e1
Dan <85998632+DKValidator@users.noreply.github.com>
2021-06-16 13:20:51 +0100

    Download binary to home directory

--- hmyupgrade.sh
+++ hmyupgrade.sh
@@ -4,6 +4,6 @@
 rm -r ~/hmybackup
 mkdir ~/hmybackup
 cp ~/harmony ~/hmybackup
-curl -LO https://harmony.one/binary && mv binary ~/harmony && chmod +x ~/harmony
+curl -LO ~/hmybinary https://harmony.one/binary && mv ~/hmybinary ~/harmony && chmod +x ~/harmony
 sudo service harmony restart
 ~harmony -V

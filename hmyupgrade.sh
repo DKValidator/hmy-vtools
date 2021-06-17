8740b05df1cd996d775c3c0c482d0c47ce8aa1b2
Dan | DK Validator <dan@dkvalidator.one>
2021-06-17 10:34:42 +0000

    Added feedback messages

--- hmyupgrade.sh
+++ hmyupgrade.sh
@@ -1,10 +1,24 @@
+
 #!/bin/bash
 # This script assumes the Harmony binary is installed as harmony in the users home directory
 
-~/harmony -V
-rm -r ~/hmybackup
-mkdir ~/hmybackup
-cp ~/harmony ~/hmybackup
-curl -LO https://harmony.one/binary && mv binary ~/harmony && chmod +x ~/harmony
+binaryDir=~
+binaryUrl=https://harmony.one/binary
+
+echo "DK Validator - Harmony upgrade script"
+echo "Current Harmony version is:"
+$binaryDir/harmony -V
+echo "Clearing backup folder..."
+rm -r $binaryDir/hmybackup
+mkdir $binaryDir/hmybackup
+echo "Backing up existing binary..."
+cp $binaryDir/harmony $binaryDir/hmybackup
+echo "Downloading lastest harmony binary from $binaryUrl..."
+curl -LO $binaryUrl
+echo "Replacing installed binary with latest..."
+mv binary $binaryDir/harmony && chmod +x $binaryDir/harmony
+echo "Restarting harmony service..."
 sudo service harmony restart
+echo "Harmony upgrade complete!"
+echo "Upgrade complete. Updated version is:"
 ~/harmony -V
